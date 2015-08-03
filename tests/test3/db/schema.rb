acfc9991b8ac9002832cd7567cbba658726acf1d
eisacke <emily@isacke.com>
2015-08-03 13:13:59 +0100

    Got categories working in test3

--- tests/test3/db/schema.rb
+++ tests/test3/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150803085244) do
+ActiveRecord::Schema.define(version: 20150803104117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,12 +24,22 @@ ActiveRecord::Schema.define(version: 20150803085244) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
   create_table "lessons", force: :cascade do |t|
     t.string   "title"
     t.integer  "teacher_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
+
+  add_index "lessons", ["category_id"], name: "index_lessons_on_category_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -37,4 +47,5 @@ ActiveRecord::Schema.define(version: 20150803085244) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "lessons", "categories"
 end
