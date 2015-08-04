02167b02dae70b0ad859c5503e27a0953492f6b6
eisacke <emily@isacke.com>
2015-08-04 10:38:21 +0100

    Added more link_tags between pages

--- skillstack/db/seeds.rb
+++ skillstack/db/seeds.rb
@@ -1,4 +1,7 @@
 u1 = User.create(first_name: "Emily", last_name: "Isacke", bio: "Graphic Designer & Developer", image: "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-atn1/v/t1.0-9/11269821_10152870891517951_1753648145167085150_n.jpg?oh=00b272d6669ea96da202424b2064ac67&oe=564028B0", password_digest: User.digest("emily").to_s, email: "emily@isacke.com")
-
 u2 = User.create(first_name: "Alex", last_name: "Chin", bio: "Full Stack Web Developer", password_digest: User.digest("alex").to_s, email: "alex@alexchin.com")
 
+c1 = Category.create(name: "Culinery", description: "Some cooking skills")
+c2 = Category.create(name: "Technology", description: "Some tech skills")
+c3 = Category.create(name: "Design", description: "Some design skills")
+c4 = Category.create(name: "DIY", description: "Some DIY skills")
