ccc9c2b3d0e8fd444c691da153d74c951dc986f3
eisacke <emily@isacke.com>
2015-08-04 09:22:28 +0100

    Changed bookings index route to only show current_user bookings

--- skillstack/app/controllers/bookings_controller.rb
+++ skillstack/app/controllers/bookings_controller.rb
@@ -4,7 +4,7 @@ class BookingsController < ApplicationController
   # GET /bookings
   # GET /bookings.json
   def index
-    @bookings = Booking.all
+    @bookings = current_user.bookings
   end
 
   # GET /bookings/1
