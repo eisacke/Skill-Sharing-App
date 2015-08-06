9c8de72a71fea9107c2f1b35a0bf35fb1457f75e
eisacke <emily@isacke.com>
2015-08-06 16:54:43 +0100

    Added styling to the bookings confirmation page

--- skillstack/app/controllers/lessons_controller.rb
+++ skillstack/app/controllers/lessons_controller.rb
@@ -1,8 +1,6 @@
 class LessonsController < ApplicationController
   before_action :set_lesson, only: [:show, :edit, :update, :destroy]
 
-  # GET /lessons
-  # GET /lessons.json
   def index
     @search = Lesson.search(params[:q])
     @lessons = @search.result
@@ -26,23 +24,17 @@ class LessonsController < ApplicationController
     redirect_to :back
   end
 
-  # GET /lessons/1
-  # GET /lessons/1.json
   def show
     @categories = Category.all
   end
 
-  # GET /lessons/new
   def new
     @lesson = Lesson.new
   end
 
-  # GET /lessons/1/edit
   def edit
   end
 
-  # POST /lessons
-  # POST /lessons.json
   def create
    params[:lesson][:teacher_id] = current_user.id
    @lesson = Lesson.new(lesson_params)
@@ -53,37 +45,28 @@ class LessonsController < ApplicationController
    end
  end
 
-  # PATCH/PUT /lessons/1
-  # PATCH/PUT /lessons/1.json
   def update
     respond_to do |format|
       if @lesson.update(lesson_params)
         format.html { redirect_to @lesson }
-        format.json { render :show, status: :ok, location: @lesson }
       else
         format.html { render :edit }
-        format.json { render json: @lesson.errors, status: :unprocessable_entity }
       end
     end
   end
-
-  # DELETE /lessons/1
-  # DELETE /lessons/1.json
+  
   def destroy
     @lesson.destroy
     respond_to do |format|
       format.html { redirect_to lessons_url }
-      format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_lesson
       @lesson = Lesson.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def lesson_params
       params.require(:lesson).permit(:title, :description, :image_one, :image_two, :image_three, :location, :cost, :teacher_id, :category_id, :city, :longitute, :latitude)
     end
